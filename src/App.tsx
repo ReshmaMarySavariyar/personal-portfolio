0082e7655f3702229e20d1500860b1762381fe5d
ReshmaMarySavariyar <reshmamary.savariyar@gmail.com>
2025-12-06 19:46:29 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Reshma Balaji</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Avaaaa {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+       
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      Click on ava u get ava from Reshma(Ur wife)
       </p>
     </>
   )
